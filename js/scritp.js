37cd0a55a0622c7ea9c5d6b01b6d03a0f80616ff
Serhii Merzhiievskyi <merzhievskiy@gmail.com>
2023-01-07 18:50:13 +0200

    first commit

---
+++ js/scritp.js
@@ -0,0 +1,8 @@
+document.addEventListener('click', burgerClick);
+
+function burgerClick(e){
+	const targetItem = e.target;
+	if (targetItem.closest('.icon-menu')){
+		document.documentElement.classList.toggle('menu-open')
+	}
+}
