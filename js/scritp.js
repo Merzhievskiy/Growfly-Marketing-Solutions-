d3a8e340e18d8e995dc443b636dae97251e63d02
Serhii Merzhiievskyi <merzhievskiy@gmail.com>
2023-01-11 12:00:46 +0200

    start craete modal window (dont work)

--- js/scritp.js
+++ js/scritp.js
@@ -1,8 +1,60 @@
+// burger
 document.addEventListener('click', burgerClick);
 
-function burgerClick(e){
+function burgerClick(e) {
 	const targetItem = e.target;
-	if (targetItem.closest('.icon-menu')){
+	if (targetItem.closest('.icon-menu')) {
 		document.documentElement.classList.toggle('menu-open')
+	}
+}
+
+
+// 
+
+// modal window
+
+const popupLinks = document.querySelectorAll('.popup-link');
+const body = document.querySelectorAll('body');
+const lockPadding = document.querySelectorAll(".lock-padding");
+
+let unlock = true;
+
+const timeout = 800;
+
+if (popupLinks.length > 0) {
+	for (let index = 0; index < popupLinks.length; index++) {
+		const popupLink = popupLinks[index];
+		popupLink.addEventListener("click", function (e) {
+			const popupName = popupLink.getAttribute('href').replace('#', '');
+			const curentPopup = document.getElementById(popupname);
+			popupOpen(curentPopup);
+			e.preventDefault();
+		})
+	}
+}
+
+const popupCloseIcon = document.querySelectorAll('.close-popup');
+if (popupCloseIcon.length > 0) {
+	for (let index = 0; index < popupCloseIcon.length; index++) {
+		const el = popupCloseIcon[index];
+		el.addEventListener('click', function (e) {
+			popupClose(el.closest('contactus__popup'));
+			e.preventDefault();
+		});
+	}
+}
+
+function popupOpen(curentPopup) {
+	if (curentPopup && unlock) {
+		const popupActive = document.querySelector('.popup.open')
+		if (popupActive) {
+			popupClose(popupActive, false);
+		}
+		curentPopup.classList.add('open');
+		curentPopup.addEventListener("click", function (e) {
+			if (!e.target.closest('.popup-contactus__content')) {
+				popupClose(e.target.closest('.popup'));
+			}
+		});
 	}
 }
