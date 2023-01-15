83f77ae3a636466127b0c8bf53863d871d99c203
Serhii Merzhiievskyi <merzhievskiy@gmail.com>
2023-01-15 15:55:16 +0200

    html css done

--- js/scritp.js
+++ js/scritp.js
@@ -14,7 +14,6 @@ function burgerClick(e) {
 // modal window
 
 const popupLinks = document.querySelectorAll('.popup-link');
-const body = document.querySelectorAll('body');
 const lockPadding = document.querySelectorAll(".lock-padding");
 
 let unlock = true;
@@ -38,7 +37,7 @@ if (popupCloseIcon.length > 0) {
 	for (let index = 0; index < popupCloseIcon.length; index++) {
 		const el = popupCloseIcon[index];
 		el.addEventListener('click', function (e) {
-			popupClose(el.closest('.popup-contactus__content'));
+			popupClose(el.closest('.contactus__popup'));
 			e.preventDefault();
 		});
 	}
@@ -49,12 +48,21 @@ function popupOpen(curentPopup) {
 		const popupActive = document.querySelector('.popup.open')
 		if (popupActive) {
 			popupClose(popupActive, false);
-		}
+		} 
 		curentPopup.classList.add('open');
 		curentPopup.addEventListener("click", function (e) {
 			if (!e.target.closest('.popup-contactus__content')) {
-				popupClose(e.target.closest('.popup'));
+				popupClose(e.target.closest('.contactus__popup'));
 			}
 		});
+	}
+}
+
+function popupClose(popupActive, doUnlock = true) {
+	if (unlock) {
+		popupActive.classList.remove('open');
+		if (doUnlock) {
+			
+		}
 	}
 }
